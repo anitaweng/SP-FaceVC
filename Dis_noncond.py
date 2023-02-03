7a560b4bf9aa386b94a546d8d2a7305a6d705673
Anita Weng <42642215+anitaweng@users.noreply.github.com>
2023-02-03 17:10:23 +0800

    Update Dis_noncond.py

--- Dis_noncond.py
+++ Dis_noncond.py
@@ -5,36 +5,23 @@ from torch.autograd import Variable
 class Dblock(nn.Module):
 	def __init__(self, in_channel, seglen, kernel_size=3):
 		super(Dblock, self).__init__()
-		'''self.spec_conv1 = nn.Conv1d(in_channel, 2*in_channel, kernel_size, padding='same')
-		self.spec_conv2 = nn.Conv1d(2*in_channel, 2*in_channel, kernel_size, padding='same')
-
-		self.cond_conv1 = nn.Conv1d(condition_channel, 2*in_channel, 1, padding='same')
-		self.cond_conv2 = nn.Conv1d(2*in_channel, condition_channel, 1, padding='same')'''
 		self.spec_conv1 = nn.utils.spectral_norm(nn.Conv1d(in_channel, 2*in_channel, kernel_size, padding=1))
 		self.spec_conv2 = nn.utils.spectral_norm(nn.Conv1d(2*in_channel, 2*in_channel, kernel_size, padding=1))
 
-		#self.cond_conv1 = nn.Conv1d(condition_channel, 2*in_channel, 1, padding=0)
-		#self.cond_conv2 = nn.Conv1d(2*in_channel, condition_channel, 1, padding=0)
-
-
 		self.lrelu = nn.LeakyReLU(0.2, True)
-		#self.layer_norm = nn.LayerNorm([2*in_channel, seglen])
 
 	def forward(self, mel):
 		# mel B*N*T
 		# cond B*256*1
 
 		spec_sc1 = self.spec_conv1(mel) # B*2N*T
-		#cond_cc1 = self.cond_conv1(cond) # B*2N*1
-		#print(spec_sc1.shape, cond_cc1.shape)
 		spec_cat = spec_sc1#+cond_cc1 # B*2N*T
 		spec_cat = self.lrelu(spec_cat)  # B*2N*T
 		spec_sc2 = self.spec_conv2(spec_cat)  # B*2N*T
 		feature = spec_sc1+spec_sc2 # B*2N*T
-		#cond_lr = self.lrelu(cond_cc1) # B*2N*1
-		#cond_out = self.cond_conv2(cond_lr) + cond # B*2N*1
+		
 
-		return feature#, cond_out
+		return feature
 
 class Discriminator(nn.Module):
 	def __init__(self, in_channel, seglen=128, kernel_size=3, num_blocks=4):
@@ -46,7 +33,6 @@ class Discriminator(nn.Module):
 			self.dblocks.add_module('dblock_{}'.format(i), Dblock(in_channel, seglen, kernel_size))
 			in_channel = 2*in_channel
 
-		#self.dblocks.add_module('projection', nn.Conv1d(in_channel, 1, kernel_size, padding='same'))
 		self.dblocks.add_module('projection', nn.utils.spectral_norm(nn.Linear(in_channel*seglen, 1)))
 
 	def forward(self, mel):
@@ -54,7 +40,6 @@ class Discriminator(nn.Module):
 		features = []
 		for i, module in enumerate(self.dblocks):
 			if i != len(self.dblocks)-1:
-				#print(mel.shape, cond.shape)
 				mel= module(mel)
 				features.append(mel.clone())
 			else:
@@ -74,7 +59,6 @@ class MultiDiscriminator(nn.Module):
 		outs = []
 		features = []
 		for module, mel in zip(self.multi_dis, mels):
-			#print(mel.shape)
 			noise = torch.rand(mel.shape[0],mel.shape[1],mel.shape[2]).cuda()
 			out, feature = module(mel+noise)
 			outs.append(out.clone())
@@ -89,9 +73,6 @@ if __name__ == "__main__":
     p = nn.AvgPool1d(3)
     x_pool = p(x)
     x_pool_ = p(x_pool)
-    #cond = Variable(torch.randn(10,256*7+4,1))
-    #cond_pool = p(cond)
-    #cond_pool_ = p(cond_pool)
     outs, features = net([x, x_pool, x_pool_])
     for out in outs:
     	print(out.shape)
