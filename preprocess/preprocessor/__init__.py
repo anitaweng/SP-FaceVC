877cf3202cac0f47b77971c549f9fe73631a2cd6
Anita Weng <42642215+anitaweng@users.noreply.github.com>
2023-02-03 22:09:39 +0800

    Delete preprocess directory

--- preprocess/preprocessor/__init__.py
+++
@@ -1,5 +0,0 @@
-import importlib
-
-def get_preprocessor(config):
-    Preprocessor = importlib.import_module(f'.{config.preprocessor_name}', package=__package__).Preprocessor
-    return Preprocessor(config)
